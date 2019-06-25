c058448aa41f8e380b44e27e18aa2357699b1b3f
Williams Gomes <williamsgomes45@gmail.com>
2019-06-25 18:26:23 -0300

    Fábricas e o problema de criação de objetos

---
+++ src/com/design/patterns/factory/ConnectionFactory.java
@@ -0,0 +1,24 @@
+package com.design.patterns.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	public Connection getConnection() {
+		Connection conexao = null;
+		try {
+			String tipoBanco = System.getenv("tipoBanco");
+			
+			if ("mysql".equals(tipoBanco))
+				conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/banco", "usuario", "senha");
+			else 
+				conexao = DriverManager.getConnection("jdbc:postgres://localhost:3306/banco", "usuario", "senha");
+			return conexao;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
